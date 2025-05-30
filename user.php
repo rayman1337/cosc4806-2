285525dea49201a3544b1776fbdf92e6a46b44fc
RM <134272733+rayman1337@users.noreply.github.com>
2025-05-30 22:20:47 +0000

    implemented create user function with dupe user checksum and password hashing

--- user.php
+++ user.php
@@ -7,7 +7,7 @@ class User {
     public function get_all_users() {
         $db = db_connect();
         if (!$db) {
-            return []; 
+           return false; 
         }
 
         $statement = $db->prepare("SELECT * FROM users;");
@@ -15,6 +15,31 @@ class User {
         $rows = $statement->fetch(PDO::FETCH_ASSOC);
         return $rows;
     }
+
+  public function create_user($username, $password) {
+      $db = db_connect();
+      if (!$db) {
+          return false; 
+      }
+
+      try {
+          $checkDupeUser = $db->prepare("SELECT COUNT(*) FROM users WHERE username = ?");
+          $checkDupeUser->execute([$username]);
+          $userExists = $checkDupeUser->fetchColumn();  
+
+          if ($userExists) {
+              return false;
+          }
+
+          $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+          $statement = $db->prepare("INSERT INTO users (username, password) VALUES (?, ?);");
+          $statement->execute([$username, $hashed_password]);
+          return $statement->rowCount();
+      } catch (PDOException $ex) {
+          error_log("Error creating user: " . $ex->getMessage());
+          return false;  
+      }
+  }
 
 }
 
